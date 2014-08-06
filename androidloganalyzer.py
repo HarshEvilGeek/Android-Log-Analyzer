b98c6c53ba6b116f56eff22e2b23b3b8ed51563d
Akhil Verghese <akhilcherian@gmail.com>
2014-08-06 09:24:14 +0530

    Starting ANR analysis

--- androidloganalyzer.py
+++ androidloganalyzer.py
@@ -11,8 +11,9 @@ key_word_agent = 'agent'
 key_word_wait = 'WAIT'
 key_word_thread_id = 'tid='
 key_word_held_by = 'held by tid='
-key_word_dalvik_threads = "DALVIK THREADS"
-key_word_native_threads = "NATIVE THREADS"
+key_word_dalvik_threads = 'DALVIK THREADS'
+key_word_native_threads = 'NATIVE THREADS'
+key_word_main = '"main"'
 
 
 def stripByteArrayString(line):
@@ -29,28 +30,71 @@ def processAnr(filename, logfile):
 	currentapp = ''
 	ourapp = False
 	issue = False
+	ismain = False
 	currentthread = -1
 	threadtype = ''
-	deadlock = False
+	deadlockoccured = False
+	deadlock = () 
+	mainwait = ''
 	threadstate = ''
 	threadwait = {}
+	threadstoprint = ()
 	threadregex = re.compile('.*".+".*prio=\d+.*tid=\d+.*')
 	for line in f:
 		line = str(line)
 		if cmd_marker in line:
 			currentapp = stripByteArrayString(line[line.index(cmd_marker) + len(cmd_marker):])
+			threadwait = {}
 			if currentapp.startswith(app_marker_name):
 				ourapp = True
 		elif key_word_dalvik_threads in line:
-			threadtype = "Dalvik"
+			threadtype = 'Dalvik'
 		elif key_word_native_threads in line:
-			threadtype = "Native"
+			threadtype = 'Native'
 		elif re.match(threadregex, line):
 			analyze = line[line.index(key_word_thread_id):]
 			currentthread = int(re.findall('\d+',analyze)[0])
 			threadstate = re.findall('[A-Z]+',analyze)[0]
-			 
-		
+			if key_word_main in line:
+				ismain = True
+				if threadstate = key_word_wait:
+					identifier = (currentapp, currentthread)
+					threadstoprint.add(identifier)
+					mainwait = currentapp
+		elif key_word_held_by in line:
+			analyze = line[line.index(key_word_held_by):]
+			heldby = int(re.findall('\d+',analyze)[0])
+			threadwait[currentthread] = heldby
+			if currentthread not = heldby and threadtype not 'Native':
+				try:
+					identifier = (currentapp, currentthread)
+					threadstoprint.add(identifier)
+					deadlock.add(identifier)
+					identifier = (currentapp, heldby)
+					threadstoprint.add(identifier)
+					deadlock.add(identifier)
+
+					while threadwait[heldby] not = heldby:
+						heldby = threadwait[heldby]
+						identifier = (currentapp, heldby)
+						threadstoprint.add(identifier)
+						deadlock.add(identifier)
+						if heldby = currentthread:
+							deadlockoccured = True
+					if not deadlockoccured
+						deadlock = ()
+				except:
+					deadlock = ()
+	
+	badopsonui = False
+	print('\nMost likely cause of ANR:')
+	if len(deadlock) > 0:
+		print('Deadlock has occured\n')
+	elif mainwait:
+		print('Main thread of ' + mainwait + ' is waiting')
+	else:
+		print('Heavy operations on UI')
+		badopsonui = True
 	
 def processLog(filename, logfile):
 	p = False
@@ -62,13 +106,14 @@ def processLog(filename, logfile):
 		if end.match(line):
 			if p:
 				print('\n\n')
-		p = 0
+				p = 0
 		if 'UNCAUGHT EXCEPTION' in line:
 			p = 1
-			print ('Uncaught Exception in file ' + filename + '\n')
+			print ('\nUncaught Exception in file ' + filename + '\n')
 		if p:
 			line = stripByteArrayString(line) 
 			print(line)
+		
 	
 def processDeviceLog(filename, logfile):
 	with logfile.open(filename, 'r') as f:
